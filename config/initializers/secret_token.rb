0bfa684cff3db6f1be7051148da0a39588f92996
Marcelo G. Jacobus Jr <marcelo.jacobus@gmail.com>
2011-06-19 19:24:58 -0300

    Commit inicial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Orcamento::Application.config.secret_token = 'b2c03b963082051648c74d5e5e8dd689adc809f8b113ce1dffd4ed0a6c25abe218e1e0ecd74dff187d981b9011b1b30785f38e997ac8d56b96e3efd6a9e9c0b5'
