fb02fb4ae9fce8a86902b46714c29faa5240f77c
Marcelo G. Jacobus Jr <marcelo.jacobus@gmail.com>
2011-06-19 20:07:01 -0300

    Adicionado validacao basica e formatacao na listagem

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+  
+  def to_currency(number)
+    number_to_currency(number, :unit => "R$ ", :separator => ",", :delimiter => ".")
+  end
+  
+  def to_date(date)
+    date.strftime("%d/%m/%Y")
+  end
+
 end
--- app/models/movimento.rb
+++ app/models/movimento.rb
@@ -1,2 +1,12 @@
 class Movimento < ActiveRecord::Base
+  validates :tipo, :presence => {:message => 'Campo obrigatório'},
+    :numericality => {:greater_than_or_equal_to => -1, :less_than_or_equal_to => 1,
+      :message => "Valor inválido"}
+  validates :valor, :presence => {:message => 'Campo obrigatório'}
+  validates :descricao, :presence => {:message => 'Campo obrigatório'}
+  
+  def validate
+    
+  end
+  
 end
