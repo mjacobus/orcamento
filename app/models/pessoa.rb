0728ca92ea34b337400f882f71534f401328afb4
Marcelo G. Jacobus Jr <marcelo.jacobus@gmail.com>
2011-06-19 22:19:52 -0300

    Adicionado validacao para pessoas

--- app/models/pessoa.rb
+++ app/models/pessoa.rb
@@ -1,2 +1,4 @@
 class Pessoa < ActiveRecord::Base
+  validates :nome, :presence => {:message => "Campo obrigatório"},
+    :uniqueness => {:case_sensitive => false, :message => "Já existe uma pessoa com este nome"}
 end
