cece4b45cf9ce7bce8e427db0bc041c0974f9708
Marcelo G. Jacobus Jr <marcelo.jacobus@gmail.com>
2011-07-03 15:58:52 -0300

    Telas traduzidas

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,4 @@
 module ApplicationHelper
-  def to_currency(number)
-    number_to_currency(number, :unit => "R$ ", :separator => ",", :delimiter => ".")
-  end
 
   def to_date(date)
     if (date != nil)
